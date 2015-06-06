86975ad494a82c76d127966618702055cd1a32f0
BlackJack7 <BlackJack7@gmail.com>
2015-06-06 13:32:02 +0300

    Adding UserController and UserControllerLogic with method name 'IsUserExists'

---
+++ StarTeam/Server/Server_Logic/UserControllerLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using StarTeam.Models;
+using StarTeam.ViewModels;
+
+namespace StarTeam.Server.Server_Logic
+{
+    public static class UserControllerLogic
+    {
+        public static UserViewModel IsUserExists(User user)
+        {
+            var usersCollection = InitMongo.Database.GetCollection<User>("Users");
+            var u = usersCollection.AsQueryable<User>().SingleOrDefault(x => x.Id.Equals(user.Id));
+            return new UserViewModel(u);
+        }
+    }
+}
